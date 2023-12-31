df509ba10acaf32dd2be48bfa4aced7c3be5b696
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2023-12-31 18:35:48 +0100

    ImageGallery

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,5 +1,6 @@
 import { SaveOutlined } from "@mui/icons-material"
 import { Grid, TextField, Typography } from "@mui/material"
+import { ImageGallery } from "../components"
 
 
 export const NoteView = () => {
@@ -7,7 +8,7 @@ export const NoteView = () => {
         <Grid container direction="row" justifyContent="space-between" alignItems="center" sx={{ mb: 1 }}>
 
             <Grid item>
-                <Typography fontSize={39} fontWeight="light"> 31 de diciembre, 2023</Typography>
+                <Typography fontSize={39} fontWeight="light"> 31th December, 2023</Typography>
             </Grid>
 
             <Grid item>
@@ -36,6 +37,8 @@ export const NoteView = () => {
                     minRows={5}
                 />
             </Grid>
+
+            <ImageGallery />
 
         </Grid>
     )
