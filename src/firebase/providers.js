990bbfb8a79bf5c8fb262931efeca275ee428f05
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-01-11 20:52:36 +0100

    create user and password

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup, updateProfile } from "firebase/auth";
 import { FirebaseAuth } from "./config";
 
 
@@ -26,6 +26,27 @@ export const signInWithGoogle = async () => {
             ok: false,
             errorMessage,
         }
+    }
+
+};
+
+
+export const registerUserWithEmailPassword = async ({ email, password, displayName }) => {
+
+    try {
+
+        const resp = await createUserWithEmailAndPassword(FirebaseAuth, email, password);
+        const { uid, photoURL } = resp.user
+
+        await updateProfile(FirebaseAuth.currentUser, { displayName });
+
+        return {
+            ok: true,
+            uid, photoURL, email, displayName
+        }
+
+    } catch (error) {
+        return { ok: false, errorMessage: error.message }
     }
 
 };
