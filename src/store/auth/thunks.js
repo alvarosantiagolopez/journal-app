c8c25c098370425600cac3ef80491d0f2c6181c1
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-01-13 17:47:49 +0100

    logout

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { signInWithGoogle, registerUserWithEmailPassword, loginWithEmailPassword } from "../../firebase/providers";
+import { signInWithGoogle, registerUserWithEmailPassword, loginWithEmailPassword, logoutFirebase } from "../../firebase/providers";
 import { checkingCredentials, login, logout } from "./";
 
 export const checkingAuthentication = (email, password) => {
@@ -40,4 +40,11 @@ export const startLoginWithEmailPassword = ({ email, password }) => {
         dispatch(login(result))
     }
 
+}
+
+export const startLogout = () => {
+    return async (dispatch) => {
+        await logoutFirebase();
+        dispatch(logout());
+    }
 }
