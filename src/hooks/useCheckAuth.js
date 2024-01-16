fd407914a908065f06a520ec9ca88ef06384ffdb
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-01-16 06:11:35 +0100

    load notes

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -4,6 +4,7 @@ import { onAuthStateChanged } from "firebase/auth";
 
 import { FirebaseAuth } from "../firebase/config";
 import { login, logout } from "../store/auth";
+import { startLoadingNotes } from "../store/journal";
 
 
 export const useCheckAuth = () => {
@@ -15,8 +16,10 @@ export const useCheckAuth = () => {
     useEffect(() => {
         onAuthStateChanged(FirebaseAuth, async (user) => {
             if (!user) return dispatch(logout());
+
             const { uid, email, displayName, photoURL } = user;
             dispatch(login({ uid, email, displayName, photoURL }));
+            dispatch(startLoadingNotes());
         })
 
     }, []);
