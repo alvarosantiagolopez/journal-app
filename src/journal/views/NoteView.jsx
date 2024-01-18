0bc9e664b957b5ee5c4114a083d938cd7ae7073e
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-01-18 06:49:56 +0100

    select files

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,14 +1,14 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 
-import { SaveOutlined } from "@mui/icons-material";
-import { Grid, TextField, Typography } from "@mui/material";
+import { SaveOutlined, UploadOutlined } from "@mui/icons-material";
+import { Grid, IconButton, TextField, Typography } from "@mui/material";
 import Swal from "sweetalert2";
 import 'sweetalert2/dist/sweetalert2.css';
 
 import { ImageGallery } from "../components";
 import { useForm } from "../../hooks/useForm";
 import { useDispatch, useSelector } from "react-redux";
-import { setActiveNote, startSaveNote } from "../../store/journal";
+import { setActiveNote, startSaveNote, startUploadingFiles } from "../../store/journal";
 
 
 export const NoteView = () => {
@@ -21,6 +21,8 @@ export const NoteView = () => {
         const newDate = new Date(date);
         return newDate.toUTCString()
     }, [date])
+
+    const fileInputRef = useRef()
 
     useEffect(() => {
         dispatch(setActiveNote(formState))
@@ -39,6 +41,11 @@ export const NoteView = () => {
         dispatch(startSaveNote());
     }
 
+    const onFileInputChange = ({ target }) => {
+        if (target.files === 0) return;
+        dispatch(startUploadingFiles(target.files))
+    }
+
 
     return (
         <Grid
@@ -55,13 +62,30 @@ export const NoteView = () => {
             </Grid>
 
             <Grid item>
+
+                <input
+                    type="file"
+                    multiple
+                    ref={fileInputRef}
+                    onChange={onFileInputChange}
+                    style={{ display: 'none' }}
+                />
+
+                <IconButton
+                    color="primary"
+                    disabled={isSaving}
+                    onClick={() => fileInputRef.current.click()}
+                >
+                    <UploadOutlined />
+                </IconButton>
+
                 <button
                     disabled={isSaving}
                     onClick={onSaveNote}
                     color="primary"
                     sx={{ padding: 2 }}
                 >
-                    <SaveOutlined sx={{ fontSize: 30, mr: 1, mt: 1 }} />
+                    <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
                     SAVE
                 </button>
             </Grid>
